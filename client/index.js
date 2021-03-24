1d592e3c9a91dacb74562de67cc2a8f43bf46f53
Lucas Sachet <lucasachet@gmail.com>
2021-03-24 20:08:10 -0300

    feat: create client config

--- client/index.js
+++ client/index.js
@@ -1,8 +1,19 @@
+/*
+node index.js
+--username pew
+--room sala101
+--hostUri localhost
+*/
+
 import Events from 'events'
+import CliConfig from './src/cliConfig.js';
 import TerminalController from "./src/terminalController.js";
 
+const [nodePath, filePath, ...commands] = process.argv
+const config = CliConfig.parseArguments(commands)
+console.log('config', config);
 
 const componentEmitter = new Events()
 
-const controller = new TerminalController()
-await controller.initializeTable(componentEmitter)
+// const controller = new TerminalController()
+// await controller.initializeTable(componentEmitter)
